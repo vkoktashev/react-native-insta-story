2be0c0b86a89e235118037d6646a0f09ac1a65d8
Vladislav Koktashev <vladislav54415@gmail.com>
2021-11-24 14:52:45 +0700

    custom avatars

--- src/Story.js
+++ src/Story.js
@@ -1,164 +1,173 @@
-import React, {Fragment, useRef, useState} from "react";
-import {LogBox, Dimensions, View, Platform} from "react-native";
+import React, { Fragment, useRef, useState } from "react";
+import { LogBox, Dimensions, View, Platform } from "react-native";
 import Modal from "react-native-modalbox";
 import StoryListItem from "./StoryListItem";
 import StoryCircleListView from "./StoryCircleListView";
-import {isNullOrWhitespace} from "./helpers/ValidationHelpers";
-import type {IUserStory} from "./interfaces/IUserStory";
+import { isNullOrWhitespace } from "./helpers/ValidationHelpers";
+import type { IUserStory } from "./interfaces/IUserStory";
 import AndroidCubeEffect from "./AndroidCubeEffect";
 import CubeNavigationHorizontal from "./CubeNavigationHorizontal";
 
 type Props = {
-    data: IUserStory[],
-    style?: any,
-    unPressedBorderColor?: string,
-    pressedBorderColor?: string,
-    onClose?: function,
-    onStart?: function,
-    duration?: number,
-    swipeText?: string,
-    customSwipeUpComponent?: any,
-    customCloseComponent?: any,
-    avatarSize?: number,
+	data: IUserStory[],
+	style?: any,
+	unPressedBorderColor?: string,
+	pressedBorderColor?: string,
+	onClose?: function,
+	onStart?: function,
+	duration?: number,
+	swipeText?: string,
+	customSwipeUpComponent?: any,
+	customCloseComponent?: any,
+	customAvatarComponent?: any,
+	avatarSize?: number,
 };
 
-LogBox.ignoreLogs(['Warning: componentWillReceiveProps']); // Ignore log notification by message
+LogBox.ignoreLogs(["Warning: componentWillReceiveProps"]); // Ignore log notification by message
 
 export const Story = (props: Props) => {
-    const {
-        data,
-        unPressedBorderColor,
-        pressedBorderColor,
-        style,
-        onStart,
-        onClose,
-        duration,
-        swipeText,
-        customSwipeUpComponent,
-        customCloseComponent,
-        avatarSize
-    } = props;
+	const {
+		data,
+		unPressedBorderColor,
+		pressedBorderColor,
+		style,
+		onStart,
+		onClose,
+		duration,
+		swipeText,
+		customSwipeUpComponent,
+		customCloseComponent,
+		avatarSize,
+	} = props;
 
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [currentPage, setCurrentPage] = useState(0);
-    const [selectedData, setSelectedData] = useState([]);
-    const cube = useRef();
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const [currentPage, setCurrentPage] = useState(0);
+	const [selectedData, setSelectedData] = useState([]);
+	const cube = useRef();
 
-    // Component Functions
-    const _handleStoryItemPress = (item, index) => {
-        const newData = data.slice(index);
-        if (onStart) {
-            onStart(item)
-        }
+	// Component Functions
+	const _handleStoryItemPress = (item, index) => {
+		const newData = data.slice(index);
+		if (onStart) {
+			onStart(item);
+		}
 
-        setCurrentPage(0);
-        setSelectedData(newData);
-        setIsModalOpen(true);
-    };
+		setCurrentPage(0);
+		setSelectedData(newData);
+		setIsModalOpen(true);
+	};
 
-    function onStoryFinish(state) {
-        if (!isNullOrWhitespace(state)) {
-            if (state == "next") {
-                const newPage = currentPage + 1;
-                if (newPage < selectedData.length) {
-                    setCurrentPage(newPage);
-                    cube?.current?.scrollTo(newPage);
-                } else {
-                    setIsModalOpen(false);
-                    setCurrentPage(0);
-                    if (onClose) {
-                        onClose(selectedData[selectedData.length - 1]);
-                    }
-                }
-            } else if (state == "previous") {
-                const newPage = currentPage - 1;
-                if (newPage < 0) {
-                    setIsModalOpen(false);
-                    setCurrentPage(0);
-                } else {
-                    setCurrentPage(newPage);
-                    cube?.current?.scrollTo(newPage);
-                }
-            }
-        }
-    }
+	function onStoryFinish(state) {
+		if (!isNullOrWhitespace(state)) {
+			if (state == "next") {
+				const newPage = currentPage + 1;
+				if (newPage < selectedData.length) {
+					setCurrentPage(newPage);
+					cube?.current?.scrollTo(newPage);
+				} else {
+					setIsModalOpen(false);
+					setCurrentPage(0);
+					if (onClose) {
+						onClose(selectedData[selectedData.length - 1]);
+					}
+				}
+			} else if (state == "previous") {
+				const newPage = currentPage - 1;
+				if (newPage < 0) {
+					setIsModalOpen(false);
+					setCurrentPage(0);
+				} else {
+					setCurrentPage(newPage);
+					cube?.current?.scrollTo(newPage);
+				}
+			}
+		}
+	}
 
-    const renderStoryList = () => selectedData.map((x, i) => {
-        return (<StoryListItem duration={duration * 1000}
-                               key={i}
-                               profileName={x.user_name}
-                               profileImage={x.user_image}
-                               stories={x.stories}
-                               currentPage={currentPage}
-                               onFinish={onStoryFinish}
-                               swipeText={swipeText}
-                               customSwipeUpComponent={customSwipeUpComponent}
-                               customCloseComponent={customCloseComponent}
-                               onClosePress={() => {
-                                   setIsModalOpen(false);
-                                   if (onClose) {
-                                       onClose(x);
-                                   }
-                               }}
-                               index={i}/>)
-    })
+	const renderStoryList = () =>
+		selectedData.map((x, i) => {
+			return (
+				<StoryListItem
+					duration={duration * 1000}
+					key={i}
+					profileName={x.user_name}
+					profileImage={x.user_image}
+					stories={x.stories}
+					currentPage={currentPage}
+					onFinish={onStoryFinish}
+					swipeText={swipeText}
+					customSwipeUpComponent={customSwipeUpComponent}
+					customCloseComponent={customCloseComponent}
+					onClosePress={() => {
+						setIsModalOpen(false);
+						if (onClose) {
+							onClose(x);
+						}
+					}}
+					index={i}
+				/>
+			);
+		});
 
-    const renderCube = () => {
-        if (Platform.OS == 'ios') {
-            return (
-                <CubeNavigationHorizontal
-                    ref={cube}
-                    callBackAfterSwipe={(x) => {
-                        if (x != currentPage) {
-                            setCurrentPage(parseInt(x));
-                        }
-                    }}
-                >
-                    {renderStoryList()}
-                </CubeNavigationHorizontal>
-            )
-        } else {
-            return (<AndroidCubeEffect
-                ref={cube}
-                callBackAfterSwipe={(x) => {
-                    if (x != currentPage) {
-                        setCurrentPage(parseInt(x));
-                    }
-                }}
-            >
-                {renderStoryList()}
-            </AndroidCubeEffect>)
-        }
-    }
+	const renderCube = () => {
+		if (Platform.OS == "ios") {
+			return (
+				<CubeNavigationHorizontal
+					ref={cube}
+					callBackAfterSwipe={(x) => {
+						if (x != currentPage) {
+							setCurrentPage(parseInt(x));
+						}
+					}}
+				>
+					{renderStoryList()}
+				</CubeNavigationHorizontal>
+			);
+		} else {
+			return (
+				<AndroidCubeEffect
+					ref={cube}
+					callBackAfterSwipe={(x) => {
+						if (x != currentPage) {
+							setCurrentPage(parseInt(x));
+						}
+					}}
+				>
+					{renderStoryList()}
+				</AndroidCubeEffect>
+			);
+		}
+	};
 
-    return (
-        <Fragment>
-            <View style={style}>
-                <StoryCircleListView
-                    handleStoryItemPress={_handleStoryItemPress}
-                    data={data}
-                    avatarSize={avatarSize}
-                    unPressedBorderColor={unPressedBorderColor}
-                    pressedBorderColor={pressedBorderColor}
-                />
-            </View>
-            <Modal
-                style={{
-                    flex: 1,
-                    height: Dimensions.get("window").height,
-                    width: Dimensions.get("window").width
-                }}
-                isOpen={isModalOpen}
-                onClosed={() => setIsModalOpen(false)}
-                position="center"
-                swipeToClose
-                swipeArea={250}
-                backButtonClose
-                coverScreen={true}
-            >
-                {renderCube()}
-            </Modal>
-        </Fragment>
-    );
+	return (
+		<Fragment>
+			<View style={style}>
+				<StoryCircleListView
+					handleStoryItemPress={_handleStoryItemPress}
+					data={data}
+					avatarSize={avatarSize}
+					unPressedBorderColor={unPressedBorderColor}
+					pressedBorderColor={pressedBorderColor}
+					customAvatarComponent={props.customAvatarComponent}
+				/>
+			</View>
+			<Modal
+				style={{
+					flex: 1,
+					height: Dimensions.get("window").height,
+					width: Dimensions.get("window").width,
+				}}
+				isOpen={isModalOpen}
+				onClosed={() => setIsModalOpen(false)}
+				position='center'
+				swipeToClose
+				swipeArea={250}
+				backButtonClose
+				coverScreen={true}
+			>
+				{renderCube()}
+			</Modal>
+		</Fragment>
+	);
 };
 export default Story;
--- src/StoryCircleListView.js
+++ src/StoryCircleListView.js
@@ -1,47 +1,51 @@
-import React, {Component} from "react";
-import {View, FlatList} from "react-native";
+import React, { Component } from "react";
+import { View, FlatList } from "react-native";
 import StoryCircleListItem from "./StoryCircleListItem";
 
 class StoryCircleListView extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
 
-    render() {
-        const {
-            data,
-            handleStoryItemPress,
-            unPressedBorderColor,
-            pressedBorderColor,
-            avatarSize
-        } = this.props;
+	render() {
+		const {
+			data,
+			handleStoryItemPress,
+			unPressedBorderColor,
+			pressedBorderColor,
+			avatarSize,
+		} = this.props;
 
-        return (
-            <View>
-                <FlatList
-                    keyExtractor={(item, index) => index.toString()}
-                    data={data}
-                    horizontal
-                    style={{paddingLeft: 12}}
-                    showsVerticalScrollIndicator={false}
-                    showsHorizontalScrollIndicator={false}
-                    ListFooterComponent={<View style={{flex: 1, width: 8}}/>}
-                    renderItem={({item, index}) => (
-                        <StoryCircleListItem
-                            avatarSize={avatarSize}
-                            handleStoryItemPress={() =>
-                                handleStoryItemPress && handleStoryItemPress(item, index)
-                            }
-                            unPressedBorderColor={unPressedBorderColor}
-                            pressedBorderColor={pressedBorderColor}
-                            item={item}
-                        />
-                    )}
-                />
-            </View>
-        );
-    }
+		return (
+			<View>
+				<FlatList
+					keyExtractor={(item, index) => index.toString()}
+					data={data}
+					horizontal
+					style={{ paddingLeft: 12 }}
+					showsVerticalScrollIndicator={false}
+					showsHorizontalScrollIndicator={false}
+					ListFooterComponent={<View style={{ flex: 1, width: 8 }} />}
+					renderItem={({ item, index }) => {
+						const props = {
+							avatarSize,
+							handleStoryItemPress: () =>
+								handleStoryItemPress && handleStoryItemPress(item, index),
+							unPressedBorderColor,
+							pressedBorderColor,
+							item,
+						};
+						if (this.props.customAvatarComponent) {
+							return <this.props.customAvatarComponent {...props} />;
+						} else {
+							return <StoryCircleListItem {...props} />;
+						}
+					}}
+				/>
+			</View>
+		);
+	}
 }
 
 export default StoryCircleListView;
